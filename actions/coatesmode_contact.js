300491da6f9128fa4124ff7dc741ce0f9e72bc46
DataFire <info@datafire.io>
2019-01-23 11:41:01 -0500

    test

--- actions/coatesmode_contact.js
+++ actions/coatesmode_contact.js
@@ -4,29 +4,16 @@ let datafire = require('datafire');
 let google_gmail = require('@datafire/google_gmail').actions;
 module.exports = new datafire.Action({
   description: "Contact Backend for Website",
-  inputs: [{
-    type: "string",
-    title: "first_name"
-  }, {
-    type: "string",
-    title: "last_name"
-  }, {
-    type: "string",
-    title: "from"
-  }, {
-    type: "string",
-    title: "message"
-  }],
   handler: async (input, context) => {
     let encodedMessage = await google_gmail.buildMessage({
       to: "james.coatesiii@gmail.com",
       from: "james.coatesiii@gmail.com",
-      subject: input.first_name + " " + input.last_name + " contacted you via Coatesmode.com",
+      subject: input.first_name + " " + input.last_name + " " + input.from + " contacted you via Coatesmode.com",
       body: input.message,
     }, context);
-    let message = await google_gmail.users.messages.send({
+    let result = await google_gmail.users.messages.send({
       userId: "james.coatesiii@gmail.com",
     }, context);
-    return message;
+    return result;
   },
 });
